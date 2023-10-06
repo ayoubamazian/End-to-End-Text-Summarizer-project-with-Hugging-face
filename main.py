74ed3f9414b125b8c05df1f15fa632ad9c980b11
Admin <Admin@PC-ADMIN>
2023-10-06 19:14:55 +0100

    data validation pipeline done

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ import sys
 from textSummarizer.logging import logging
 from textSummarizer.exceptions import CustomException
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainPipeline
+from textSummarizer.pipeline.stage_02_data_validation import DataValidationPipeline
 
 STAGE_NAME = 'Data Ingestion Stage'
+STAGE_NAME_2 = 'Data Validation Stage'
 try:
     logging.info(f"Stage {STAGE_NAME} started")
     data_ingestion = DataIngestionTrainPipeline()
     data_ingestion.main()
     logging.info(f"Stage {STAGE_NAME} completed")
+    
+    logging.info(f"Stage {STAGE_NAME_2} started")
+    data_validation = DataValidationPipeline()
+    data_validation.main()
+    logging.info(f"Stage {STAGE_NAME_2} completed")
 
 except Exception as e:
     raise CustomException(e, sys)
